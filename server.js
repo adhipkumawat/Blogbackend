19b4f665c618d5f7ae899c0e7739e06050295a0f
Adhip <adhipkumawat@gmail.com>
2026-04-02 18:29:56 +0530

    f

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors({
   origin: allowedOrigins,
   credentials: true
 }));
-app.options("/*", cors({ origin: allowedOrigins, credentials: true }));
+app.options(/.*/, cors({ origin: allowedOrigins, credentials: true }));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
