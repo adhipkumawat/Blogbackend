87e6e6e4268e6cf33864a8057f7567d4ecf0c91a
Adhip <adhipkumawat@gmail.com>
2026-04-06 11:16:30 +0530

    w]

--- Blogcall.js
+++ Blogcall.js
@@ -192,5 +192,59 @@ router.delete("/posts/:id", async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 });
+router.get("/all-comments", async (req, res) => {
+  try {
+    const posts = await Post.find().populate("author");
+
+    let allComments = [];
+
+    posts.forEach(post => {
+      post.comments.forEach(comment => {
+        allComments.push({
+          text: comment.text,
+          user: comment.user,
+          blogTitle: post.title,
+           postId: post._id,       
+          commentId: comment._id, 
+          blogId: post._id,
+          createdAt: comment.createdAt
+        });
+      });
+    });
+
+    res.json(allComments);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+router.delete("/delete-comment", async (req, res) => {
+  try {
+    const { postId, commentId } = req.body;
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) return res.status(401).json({ message: "No token" });
+
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, "SECRETKEY123");
+
+    // ✅ ONLY ADMIN
+    if (decoded.role !== "admin") {
+      return res.status(403).json({ message: "Access denied" });
+    }
+
+    const post = await Post.findById(postId);
+
+    post.comments = post.comments.filter(
+      (c) => c._id.toString() !== commentId
+    );
+
+    await post.save();
+
+    res.json({ message: "Comment deleted" });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 module.exports = router;
